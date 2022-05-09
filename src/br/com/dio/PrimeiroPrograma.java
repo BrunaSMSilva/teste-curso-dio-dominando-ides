312ec73f6b075b7e243574077c008ff85b5c4b7a
BrunaSMSilva <brubs_sousamoraes@hotmail.com>
2022-05-09 08:43:36 -0300

    Primeiro commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,18 +8,15 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		
 		Gato gato = new Gato();
-		Livro livro = new Livro ();
+		
 		
 		System.out.println (gato);	
-		System.out.println (livro);
+	
 		/*int a = 2;
 		int b = 3;
 		System.out.println("Hello World! " + (a+b));*/
 
 	}
-		public class Livro () { 
-		public String livro;
-		public Integer numPaginas; 
 	
 		}
 
